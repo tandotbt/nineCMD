d067c63a543840e7d27ae08eb450e5a522bc2857
tandotbt <tan.dot.bt@gmail.com>
2024-11-15 00:35:37 +0700

    Ngăn trùng avatarAddress từ Mimir
    - Dữ liệu Mimir có sự trùng avatarAddress, cập nhật này sẽ lấy dữ liệu của avatar address lớn hơn

--- src/utilities/convertToArenaParticipants.js
+++ src/utilities/convertToArenaParticipants.js
@@ -1,22 +1,23 @@
 export function convertToArenaParticipants(leaderboardByAvatarAddress, my_score) {
-  let CPNext = 1
-  let arenaParticipants = leaderboardByAvatarAddress.map((item) => {
-    const avatarAddress = item.simpleAvatar.address
-    const hashedPart = avatarAddress.slice(2, 6)
-    const nameWithHash = `${item.simpleAvatar.name} <size=80%><color=#A68F7E>#${hashedPart}</color></size>`
-    // Tính winScore
-    let winScore = 0
-    const score = item.arenaScore.score
+  let CPNext = 1;
+  const uniqueParticipants = {};
+
+  leaderboardByAvatarAddress.forEach((item) => {
+    const avatarAddress = item.simpleAvatar.address;
+    const hashedPart = avatarAddress.slice(2, 6);
+    const nameWithHash = `${item.simpleAvatar.name} <size=80%><color=#A68F7E>#${hashedPart}</color></size>`;
+    let winScore = 0;
+    const score = item.arenaScore.score;
 
     if (score >= my_score) {
-      winScore = 20
+      winScore = 20;
     } else if (score > my_score - 100) {
-      winScore = 18
+      winScore = 18;
     } else {
-      winScore = 16
+      winScore = 16;
     }
-    CPNext = CPNext + 1
-    return {
+
+    const participant = {
       avatarAddr: avatarAddress,
       score: item.arenaScore.score,
       rank: item.rank,
@@ -25,8 +26,19 @@ export function convertToArenaParticipants(leaderboardByAvatarAddress, my_score)
       cp: CPNext,
       portraitId: 10200000,
       level: item.simpleAvatar.level,
-      nameWithHash
+      nameWithHash,
+    };
+    // Dữ liệu mimir trả về bị lặp avatarAddress, ngăn trùng avatar
+    if (
+      !uniqueParticipants[avatarAddress] ||
+      uniqueParticipants[avatarAddress].score < score
+    ) {
+      uniqueParticipants[avatarAddress] = participant;
     }
-  })
-  return arenaParticipants
+
+    CPNext++;
+  });
+
+  return Object.values(uniqueParticipants);
 }
+
